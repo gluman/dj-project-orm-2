8795cd04cd5f05c2f54c3a1ca408ef35e7625833
gluman <gluman@yandex.ru>
2023-06-19 00:37:26 +0300

    19/06/2023 доделал импорт. Далее нужнос делать вывод на сайте

--- school/models.py
+++ school/models.py
@@ -5,12 +5,12 @@ class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
 
-    # class Meta:
-    #     verbose_name = 'Учитель'
-    #     verbose_name_plural = 'Учителя'
-    #
-    # def __str__(self):
-    #     return self.name
+    class Meta:
+        verbose_name = 'Учитель'
+        verbose_name_plural = 'Учителя'
+
+    def __str__(self):
+        return self.name
 
 
 class Student(models.Model):
@@ -18,13 +18,13 @@ class Student(models.Model):
     teachers = models.ManyToManyField(Teacher, through='Releations')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
-    # class Meta:
-    #     verbose_name = 'Ученик'
-    #     verbose_name_plural = 'Ученики'
-    #
-    # def __str__(self):
-    #
-    #     return self.name
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+
+    def __str__(self):
+
+        return self.name
 class Releations(models.Model):
     students = models.ForeignKey(Student, on_delete=models.CASCADE)
     teachers = models.ForeignKey(Teacher, on_delete=models.CASCADE)
